e2078ed0fddeb05bb262b5cbeaaf6702c309b52a
Aven9 <961273881@qq.com>
2019-06-05 22:31:46 +0800

    simple login, create channel, send message

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -10,4 +10,16 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
+
+
+# @socketio.on('join')
+# def on_join(data):
+#     username = data.username
+#     room = data['room']
+#     join_room(room)
+
+
+@socketio.on('message')
+def handle_message(message):
+    emit('response', message, broadcast=True)
